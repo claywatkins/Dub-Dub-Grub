54db52bb203d3c9c319773cfae7aff50dc2e31b4
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-06-16 12:35:17 -0700

    Added Project

---
+++ Dub Dub Grub/Dub Dub Grub/Dub_Dub_GrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dub_Dub_GrubApp.swift
+//  Dub Dub Grub
+//
+//  Created by Clayton Watkins on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Dub_Dub_GrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
