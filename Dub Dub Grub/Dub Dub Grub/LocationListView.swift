c597869977e6751726ea884ae728718585a7feb3
Clayton Watkins <watkinsclayton17@yahoo.com>
2021-06-16 18:15:35 -0700

    Moved cell view into a LocationViewCell, adjusted AvatarView to accept an optional name

--- Dub Dub Grub/Dub Dub Grub/LocationListView.swift
+++ Dub Dub Grub/Dub Dub Grub/LocationListView.swift
@@ -12,27 +12,7 @@ struct LocationListView: View {
         NavigationView {
             List(0..<10) { _ in
                 NavigationLink(destination: LocationDetailView()) {
-                    HStack{
-                        Image("default-square-asset")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            .clipShape(Circle())
-                            .padding(.vertical, 8)
-                        VStack(alignment: .leading, spacing: 15) {
-                            Text("Resturant Name")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.75)
-                            HStack {
-                                ForEach(0..<5) { _ in
-                                    AvatarView()
-                                }
-                            }
-                        }
-                        .padding(.leading, 8)
-                    }
+                    LocationCell()
                 }
                 
             }
@@ -48,11 +28,48 @@ struct LocationListView_Previews: PreviewProvider {
 }
 
 struct AvatarView: View {
+    var size: CGFloat
+    var name: String?
+    
     var body: some View {
-        Image("default-avatar")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 35, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            .clipShape(Circle())
+        VStack {
+            Image("default-avatar")
+                .resizable()
+                .scaledToFit()
+                .frame(width: size, height: size)
+                .clipShape(Circle())
+            if let name = name {
+                Text(name)
+                    .bold()
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+            }
+        }
+    }
+}
+
+struct LocationCell: View {
+    var body: some View {
+        HStack{
+            Image("default-square-asset")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .clipShape(Circle())
+                .padding(.vertical, 8)
+            VStack(alignment: .leading, spacing: 15) {
+                Text("Resturant Name")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                HStack {
+                    ForEach(0..<5) { _ in
+                        AvatarView(size: 35)
+                    }
+                }
+            }
+            .padding(.leading, 8)
+        }
     }
 }
